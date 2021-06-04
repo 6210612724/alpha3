0e87f6eb03473d2dc731aa1107c707308de00c04
Thanawin Srisuk <thanawin.sri@dome.tu.ac.th>
2021-06-04 11:49:41 +0700

    every 15 mins

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def process(use_infor):
     if order[0] == "อัพเดต":
         i = 0
         money = float(order[2])
-        while i < 8:
+        while i < 226:
             line_bot_api.push_message(use_infor[0], TextSendMessage(text=cost(money)))
             time.sleep(4)
             i += 1
